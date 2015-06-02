1bdb277b4f822c52020fdac7e2935fad3316d6f2
Zachary Jones <zacharytamas@gmail.com>
2015-06-02 19:55:20 -0400

    Uncommenting their tests as I was told. At this point we have a failing test so this begins a TDD cycle.

--- app/src/main/java/com/zacharytamas/sunshine/app/data/WeatherDbHelper.java
+++ app/src/main/java/com/zacharytamas/sunshine/app/data/WeatherDbHelper.java
@@ -39,11 +39,6 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         final String SQL_CREATE_WEATHER_TABLE = "CREATE TABLE " + WeatherEntry.TABLE_NAME + " (" +
-                // Why AutoIncrement here, and not above?
-                // Unique keys will be auto-generated in either case.  But for weather
-                // forecasting, it's reasonable to assume the user will want information
-                // for a certain date and all dates *following*, so the forecast data
-                // should be sorted accordingly.
                 WeatherEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
 
                 // the ID of the location entry associated with this weather data
