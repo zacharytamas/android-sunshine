a6c8f7a9ecc4444277a85f4681cb6f630e1ada2f
Zachary Jones <zacharytamas@gmail.com>
2015-06-02 20:03:27 -0400

    Turns out I missed this. I'm not used to dealing this low level with SQL...

--- app/src/main/java/com/zacharytamas/sunshine/app/data/WeatherDbHelper.java
+++ app/src/main/java/com/zacharytamas/sunshine/app/data/WeatherDbHelper.java
@@ -66,7 +66,7 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
 
         final String SQL_CREATE_LOCATION_TABLE = "CREATE TABLE " + LocationEntry.TABLE_NAME + " (" +
                 LocationEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                LocationEntry.COLUMN_LOCATION_SETTING + " TEXT NOT NULL, " +
+                LocationEntry.COLUMN_LOCATION_SETTING + " TEXT UNIQUE NOT NULL, " +
                 LocationEntry.COLUMN_CITY_NAME + " TEXT NOT NULL, " +
                 LocationEntry.COLUMN_COORD_LAT + " REAL NOT NULL, " +
                 LocationEntry.COLUMN_COORD_LONG + " REAL NOT NULL);";
