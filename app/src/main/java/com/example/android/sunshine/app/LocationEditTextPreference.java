584c0da9e0bf4e731b8d150168e1032f780ed80b
Zachary Jones <zacharytamas@gmail.com>
2015-06-15 19:13:01 -0400

    Enforce minLength and add a11y enhancements.

--- app/src/main/java/com/example/android/sunshine/app/LocationEditTextPreference.java
+++ app/src/main/java/com/example/android/sunshine/app/LocationEditTextPreference.java
@@ -1,39 +1,72 @@
 package com.example.android.sunshine.app;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.Bundle;
 import android.preference.EditTextPreference;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
-import android.util.Log;
-import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by zacharytamas on 6/15/15.
  */
 public class LocationEditTextPreference extends EditTextPreference {
-    public static final int DEFAULT_MINIMUM_LOCATION_LENGTH = 3;
-    private Integer mMinLength;
+    static final private int DEFAULT_MINIMUM_LOCATION_LENGTH = 2;
+    private int mMinLength;
 
     public LocationEditTextPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
-
-        TypedArray a = context.getTheme().obtainStyledAttributes(attrs,
-                R.styleable.LocationEditTextPreference, 0, 0);
-
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.LocationEditTextPreference,
+                0, 0);
         try {
-            mMinLength = a.getInteger(R.styleable.LocationEditTextPreference_minLength,
-                    DEFAULT_MINIMUM_LOCATION_LENGTH);
+            mMinLength = a.getInteger(R.styleable.LocationEditTextPreference_minLength, DEFAULT_MINIMUM_LOCATION_LENGTH);
         } finally {
             a.recycle();
         }
     }
 
+
     @Override
-    protected View onCreateView(ViewGroup parent) {
+    protected void showDialog(Bundle state) {
+        super.showDialog(state);
 
-        Log.i("LocationEditText", mMinLength.toString());
+        EditText et = getEditText();
+        et.addTextChangedListener(new TextWatcher() {
 
-        return super.onCreateView(parent);
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Dialog d = getDialog();
+                if (d instanceof AlertDialog) {
+                    AlertDialog dialog = (AlertDialog) d;
+                    Button positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                    // Check if the EditText is empty
+                    if (s.length() < mMinLength) {
+                        // Disable OK button
+                        positiveButton.setEnabled(false);
+                    } else {
+                        // Re-enable the button.
+                        positiveButton.setEnabled(true);
+                    }
+                }
+            }
+        });
     }
 }
