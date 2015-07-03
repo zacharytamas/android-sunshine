a57be7c7314874325d93dbe2cf335499b2d2b45e
Zachary Jones <zacharytamas@gmail.com>
2015-07-03 09:52:12 -0400

    Hook up widget and make it work!

---
+++ app/src/main/java/com/example/android/sunshine/app/TodayWidget.java
@@ -0,0 +1,32 @@
+package com.example.android.sunshine.app;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class TodayWidget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        final int N = appWidgetIds.length;
+        for (int i = 0; i < N; i++) {
+            updateAppWidget(context, appWidgetManager, appWidgetIds[i]);
+        }
+    }
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_small);
+        views.setTextViewText(R.id.degreeTextView, "77");
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+}
+
